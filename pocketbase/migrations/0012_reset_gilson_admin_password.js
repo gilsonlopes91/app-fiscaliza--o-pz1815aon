7c0b3060f4353cc9a42fb9748cc6b9838eedbe4f
gilsonlopes91 <229774161+gilsonlopes91@users.noreply.github.com>
2026-09-02 16:27:03 -0300

    Auto-commit before sync

--- pocketbase/migrations/0012_reset_gilson_admin_password.js
+++ pocketbase/migrations/0012_reset_gilson_admin_password.js
@@ -8,9 +8,21 @@ migrate(
       app.save(users)
     }
 
-    let user = app.findFirstRecordByData('_pb_users_auth_', 'id', '9d79w7s8tobmghd')
-    user.setPassword('Skip@2026')
-    app.save(user)
+    let user
+    try {
+      user = app.findFirstRecordByData('_pb_users_auth_', 'id', '9d79w7s8tobmghd')
+    } catch (_) {
+      try {
+        user = app.findAuthRecordByEmail('_pb_users_auth_', 'gilsonlopes2991@gmail.com')
+      } catch (err) {
+        // Fallback if not found
+      }
+    }
+
+    if (user) {
+      user.setPassword('Skip@2026')
+      app.save(user)
+    }
   },
   (app) => {
     // Revert logic
